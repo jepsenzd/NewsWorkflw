d915a13ca013f88e4f80cce2d476fbbbc43e3ca3
Administrator <Administrator@DESKTOP-099UQRG>
2019-11-05 13:04:19 -0500

    Getting closer to making this work.. I hope.

--- src/main/java/com/NewsWorkflw/common/controller/RssController.java
+++ src/main/java/com/NewsWorkflw/common/controller/RssController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.NewsWorkflw.common.model.SampleContent;
@@ -13,7 +15,7 @@ import com.NewsWorkflw.common.model.SampleContent;
 @Controller
 public class RssController {
 
-	@RequestMapping(value="/rssfeed", method = RequestMethod.GET)
+	/*@RequestMapping(value="/", method = RequestMethod.GET)
 	public ModelAndView getFeedInRss() {
 
 		List<SampleContent> items = new ArrayList<SampleContent>();
@@ -32,11 +34,39 @@ public class RssController {
 		content2.setCreatedDate(new Date());
 		items.add(content2);
 		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("rssViewer");
-		mav.addObject("feedContent", items);
+		ModelAndView modelview = new ModelAndView();
+		modelview.setViewName("rssViewer");
+		modelview.addObject("feedContent", items);
 		
-		return mav;
+		return modelview;
+
+	}*/
+	@RequestMapping(value="/rssfeed", method = RequestMethod.GET)
+	//@ResponseBody
+	public String rssFeed(Model model) {
+
+		List<SampleContent> items = new ArrayList<SampleContent>();
+		
+		SampleContent content  = new SampleContent();
+		content.setTitle("Spring MVC Tutorial 1");
+		content.setUrl("http://www.mkyong.com/spring-mvc/tutorial-1");
+		content.setSummary("Tutorial 1 summary ...");
+		content.setCreatedDate(new Date());
+		items.add(content);
+		
+		SampleContent content2  = new SampleContent();
+		content2.setTitle("Spring MVC Tutorial 2");
+		content2.setUrl("http://www.mkyong.com/spring-mvc/tutorial-2");
+		content2.setSummary("Tutorial 2 summary ...");
+		content2.setCreatedDate(new Date());
+		items.add(content2);
+		
+		
+		
+		
+		model.addAttribute("feedContent", items);
+		
+		return "rssfeed";
 
 	}
 	

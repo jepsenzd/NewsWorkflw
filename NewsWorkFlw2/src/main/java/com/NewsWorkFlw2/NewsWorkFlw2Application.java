6e86355b97f42c80e36a99af99ba1e11a1714c48
Andrew Petersen <peteraw@mail.uc.edu>
2019-11-06 19:48:11 -0700

    edit dependencies + main.jsp changes

---
+++ NewsWorkFlw2/src/main/java/com/NewsWorkFlw2/NewsWorkFlw2Application.java
@@ -0,0 +1,13 @@
+package com.NewsWorkFlw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWorkFlw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWorkFlw2Application.class, args);
+	}
+
+}
---
+++ NewsWorkFlw2/src/test/java/com/NewsWorkFlw2/NewsWorkFlw2/NewsWorkFlw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewsWorkFlw2.NewsWorkFlw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWorkFlw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
